cb74b4b14d9f1e118e6add78f49de850adf35d8f
k2miha <k2punk@gmail.com>
2019-11-26 10:45:40 +0300

    copypaste alien code(don't understand how it work)

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  fns.map(x => {
+    if (typeof x !== 'function') throw new Error('It is not function');
+  });
+  return x => fns.reduce((v, f) => f(v), x);
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  const events = {};
+  const x = num => fns.reverse().reduce((v, f) => {
+    try {
+      if (typeof v === 'undefined') return undefined;
+      return f(v);
+    } catch (e) {
+      const err = events['error'];
+      if (err) err(e);
+      return undefined;
+    }
+  }, num);
+  x.on = (event, callback) => {
+    events[event] = callback;
+  };
+  return x;
+};
 
 module.exports = { compose };
